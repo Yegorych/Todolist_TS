c22dc888b8218666e2bac9237522bf2c89380ea7
Egorych <mikhalevichegor@gmail.com>
2021-02-17 19:43:03 +0300

    Todolist - TS - 02. Делаем Todolist на TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./components/Todolist/Todolist";
+import {Todolist} from "./Todolist/Todolist";
+
 
 function App() {
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
     return (
         <div className="App">
             <div>
-                <Todolist/>
-                <Todolist/>
-                <Todolist/>
-                <Todolist/>
+                <Todolist title="What to learn" tasks={tasks1} />
+                <Todolist title="Songs" tasks={tasks2} />
+
             </div>
         </div>
     );
