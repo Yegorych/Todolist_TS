7fb77b83db2d1067a6f33b34bb05100c936e734b
Egorych <mikhalevichegor@gmail.com>
2021-02-17 19:25:26 +0300

    Todolist - TS - 01 - components Сщздали компоненту Todolist и вынесли из app Header, Footer, List в Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header/Header';
-import {Footer} from "./components/Footer/Footer";
+import {Todolist} from "./components/Todolist/Todolist";
 
 function App() {
     return (
         <div className="App">
             <div>
-                <Header/>
                 <Todolist/>
-                <Footer/>
+                <Todolist/>
+                <Todolist/>
+                <Todolist/>
             </div>
         </div>
     );
@@ -19,29 +19,5 @@ export default App;
 
 
 
-function Todolist() {
-    return (
-        <div className="todoList">
-            <div className="todoList-tasks">
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true}/>
-                    <span>CSS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false}/>
-                    <span>JS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false}/>
-                    <span>ReactJS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true}/>
-                    <span>Patterns</span>
-                </div>
-            </div>
 
-        </div>
-    )
-}
 
