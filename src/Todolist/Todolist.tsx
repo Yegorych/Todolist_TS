425905a014a9bd016c88f9fcb81ad8808b7598ac
Egorych <mikhalevichegor@gmail.com>
2021-02-19 20:16:36 +0300

    Todolist - TS - 02. отрисовали jsx-элементы на основе массива map.

--- src/Todolist/Todolist.tsx
+++ src/Todolist/Todolist.tsx
@@ -20,9 +20,11 @@ export function Todolist(props: PropsType) {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+                {props.tasks.map(t => <li key={t.id}>
+                    <input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span></li>
+                )
+                }
             </ul>
             <div>
                 <button>All</button>
