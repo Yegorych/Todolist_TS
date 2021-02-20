701b856800dac3c517eb345de4375e6d83271d18
Egorych <mikhalevichegor@gmail.com>
2021-02-20 23:16:27 +0300

    Todolist - TS - 02. добавили checkbox для фильтров, с помощью hook, и настроили отображение данных на основе значения указанного фильтра.

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,30 @@ function App() {
             { id: 1, title: "HTML&CSS", isDone: true },
             { id: 2, title: "JS", isDone: true },
             { id: 3, title: "ReactJS", isDone: false },
-        ]
-    )
+        ])
+    let [filter, setFilter] = useState("all")
 
     function removeTask(id: number) {
         tasks = tasks.filter(t => t.id !== id)
         setTasks(tasks)
     }
+    let tasksForTodolist = tasks;
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true)
+    }
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false)
+    }
+
+
+
 
     return (
         <div className="App">
             <div>
                 <Todolist
                     title="What to learn"
-                    tasks={tasks}
+                    tasks={tasksForTodolist}
                     removeTask={removeTask}
                 />
             </div>
