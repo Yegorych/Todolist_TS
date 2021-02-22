75eacc4014e673e68d1bcf4cee39177bcee6d542
Egorych <mikhalevichegor@gmail.com>
2021-02-22 16:44:52 +0300

    Todolist - TS - 03. При вводе нового значения в input, занулили его.

--- src/Todolist/Todolist.tsx
+++ src/Todolist/Todolist.tsx
@@ -24,16 +24,23 @@ export function Todolist(props: PropsType) {
         <div className={"todolist"}>
             <h3>{props.title}</h3>
             <div>
-                <input value={newTaskTitle} onChange={ (e) => {setNewTaskTitle(e.currentTarget.value)}}
-
+                <input value={newTaskTitle}
+                       onChange={(e) => {
+                           setNewTaskTitle(e.currentTarget.value)}}
                 />
-                <button onClick={ () => {props.addTask(newTaskTitle)}}>+</button>
+                <button onClick={ () => {
+                    props.addTask(newTaskTitle)
+                    setNewTaskTitle('')
+                }}>+</button>
             </div>
             <ul>
                 {props.tasks.map(t => <li key={t.id}>
                         <input type="checkbox" checked={t.isDone}/>
                         <span>{t.title}</span>
-                        <button onClick={ () => { props.removeTask(t.id)}}>x</button>
+                        <button onClick={ () => {
+                            props.removeTask(t.id)
+
+                        }}>x</button>
                     </li>
                 )}
             </ul>
