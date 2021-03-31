4e0a092dbe2d5575db6a0a82e3cef138ca4ffaf4
Egorych <mikhalevichegor@gmail.com>
2021-03-31 20:20:07 +0300

    todolist 08-04 Создал RemoveTodolistActionType и AddTodolistActionType  в todolists-reducer.ts

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -4,11 +4,20 @@ type RemoveTodolistActionType = {
     type: 'REMOVE-TODOLIST'
     id: string
 }
+type AddTodolistActionType = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
 
-type ActionType = RemoveTodolistActionType
+export type ActionType = RemoveTodolistActionType | AddTodolistActionType
 
 export const todolistReducer = (state: Array<TodolistType>, action: ActionType) => {
     switch (action.type) {
-        case:
+        case 'REMOVE-TODOLIST' :
+            return state.filter(tl => tl.id != action.id)
+        case 'ADD-TODOLIST' :
+            return
+        default:
+            throw new Error('I don`t understand this type')
     }
 }
