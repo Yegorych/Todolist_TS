290d06c0f3f6e768dbc456c04553e0ea2cbf3230
Egorych <mikhalevichegor@gmail.com>
2021-03-31 21:04:22 +0300

    todolist 08-06 Создал CHANGE-TODOLIST-FILTER

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,4 +1,4 @@
-import {TodolistType} from "../App";
+import {FilterValueType, TodolistType} from "../App";
 import {v1} from "uuid";
 
 type RemoveTodolistActionType = {
@@ -14,28 +14,43 @@ type ChangeTitleActionType = {
     id: string
     title: string
 }
+type ChangeFilterActionType = {
+    type: 'CHANGE-FILTER'
+    id: string
+    filter: FilterValueType
+}
 
-export type ActionType = RemoveTodolistActionType | AddTodolistActionType | ChangeTitleActionType
+export type ActionType = RemoveTodolistActionType | AddTodolistActionType | ChangeTitleActionType | ChangeFilterActionType
 
 export const todolistReducer = (state: Array<TodolistType>, action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST' :
             return state.filter(tl => tl.id != action.id)
-        case 'ADD-TODOLIST' :
+        case 'ADD-TODOLIST' :{
             let newTodolistId = v1()
-            let newTodolist: TodolistType = {
+            let todolist: TodolistType = {
                 id: newTodolistId,
                 title: action.title,
                 filter: "all"
             }
-            return [newTodolist, ...state]
-        case 'CHANGE-TODOLIST-TITLE':
+            return [todolist, ...state]
+        }
+        case 'CHANGE-TODOLIST-TITLE': {
             const todolist = state.find(tl => tl.id === action.id)
             if (todolist) {
                 todolist.title = action.title
                 return [...state]
             }
             return state
+        }
+        case "CHANGE-FILTER": {
+            let todolist = state.find(tl => tl.id === action.id)
+            if (todolist) {
+                todolist.filter = action.filter
+                return[...state]
+            }
+            return state
+        }
         default:
             return state
     }
